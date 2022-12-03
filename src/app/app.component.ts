9d404dd9e698646a29bdac4ef5559d03a5a04798
RolyDev <rolyanddev@gmail.com>
2022-12-02 23:16:25 -0500

    properties, events ans ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Pedro';
+  age = 23;
+  image = 'https://source.unsplash.com/random';
+  isButtonDisable = true;
+  person = {
+    name: 'Pedro Angel',
+    age: 23,
+    avatar: 'https://source.unsplash.com/random',
+  };
+
+  toggleButton() {
+    this.isButtonDisable = !this.isButtonDisable;
+  }
+
+  increaseAge() {
+    this.person.age++;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
